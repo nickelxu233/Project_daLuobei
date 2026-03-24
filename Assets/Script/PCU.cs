8ab0951fb024954091e50ee00017b1e829cd00ad
nickelxu233 <nickelxu233@gmail.com>
2026-03-24 23:27:46 +0800

    Day1

---
+++ Assets/Script/PCU.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCU : MonoBehaviour
+{
+    public Material mt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
